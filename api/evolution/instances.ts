c5521e0c68a90b3378593ac93aab7a8601e0b4c7
jhonygeni <jhony@geni.chat>
2025-06-07 10:16:45 -0300

    any para os paramentros esmodule

--- api/evolution/instances.ts
+++ api/evolution/instances.ts
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Método não permitido' });
   }
